c47386d0722a1fa99db60db1c7d77368d51db8bb
Zipen999 <78689752+Zipen999@users.noreply.github.com>
2021-11-24 15:23:56 +0100

    update proto

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
-DROP TABLE Approvisione;
+DROP TABLE Approvisioner;
 DROP TABLE Fournisseur;
 DROP TABLE Existe;
 DROP TABLE Stock;
@@ -6,28 +6,34 @@ DROP TABLE Achete;
 DROP TABLE Client;
 DROP TABLE Magasin;
 DROP TABLE Produit;
-DROP TABLE Categorie;	
+DROP TABLE Categorie;
 
-CREATE TABLE Fournisseur(
-	idFournisseur 	char(5) PRIMARY KEY,
-	nom 			varchar(30) NOT NULL,
-	rue 			varchar(30) NOT NULL,
-	ville 			varchar(40) NOT NULL,
-	pays 			char(42) NOT NULL CONSTRAINT ck_pays CHECK (NOT(pays = 'France' AND codeP IS NOT NULL)),
-	codeP 			numeric(5) NOT NULL,
-	tel  			numeric(10) UNIQUE NOT NULL,
-	email			varchar(45)
+
+CREATE TABLE Categorie(
+	idCategorie				char(5) PRIMARY KEY,
+	nom  					varchar(30) UNIQUE NOT NULL,
+	CONSTRAINT ck_categorie CHECK (nom IN('Foot','Rugby','Natation','Boxe','Hokey','Tennis'))
 );
 
 CREATE TABLE Produit(
 	idProduit 		char(5) PRIMARY KEY,
-	nom 			varchar(30) NOT NULL,
-	type 			varchar(30) NOT NULL CONSTRAINT ck_type CHECK (type IN ('Tête', 'Haut', 'Bas', 'Chaussure', 'Accessoire'),
-	taille 			varchar(3) NOT NULL CONSTRAINT ck_taille CHECK (type !='Chaussure' AND taille IN('S','M','L','XL','XXL')) OR (type ='Chaussure', AND taille >=16 AND taille <= 55),
+	nom 			varchar(30) NOT NULL UNIQUE,
+	type 			varchar(30) NOT NULL CONSTRAINT ck_type CHECK (type IN('Tête','Haut','Bas','Chaussure','Accessoire')),
+	taille 			varchar(3) NOT NULL CONSTRAINT ck_taille CHECK (type !='Chaussure' AND taille IN('S','M','L','XL','XXL')) OR (type ='Chaussure' AND taille >= 16 AND taille <= 55),
 	marque 			varchar(30) NOT NULL,
-	prix 			numeric(5,2) NOT NULL CONSTRAINT ck_prix CHECK (prix > 0),
-	idCategorie		char(5),
-	FOREIGN KEY (idCategorie) REFERENCES Categorie(idCategorie)
+	prix 			numeric(5,2) NOT NULL CONSTRAINT ck_prix CHECK ( prix > 0),
+	categorie 		char(5),
+	CONSTRAINT fk_categorie FOREIGN KEY (categorie) REFERENCES Categorie(idCategorie)
+);
+
+CREATE TABLE Magasin(
+	idMagasin			char(5) PRIMARY KEY,
+	rue 				varchar(30),
+	ville				varchar(40),
+	codeP				numeric(5),
+	pays 				varchar(42) NOT NULL CONSTRAINT ck_pays CHECK (NOT(pays ='France' AND (rue,ville,codeP) IS NOT NULL)),
+	tel 				numeric(15) UNIQUE NOT NULL,
+	email 				varchar(45) UNIQUE
 );
 
 CREATE TABLE Client(
@@ -35,61 +41,57 @@ CREATE TABLE Client(
 	nom 				varchar(30) NOT NULL,
 	prenom 				varchar(30) NOT NULL,
 	date_naissance 		date NOT NULL,
-	rue 				varchar(30) NOT NULL,
-	ville 				varchar(30) NOT NULL,
-	codeP 				numeric(5) NOT NULL,
-	pays 				varchar(30) NOT NULL,
+	rue 				varchar(30),
+	ville 				varchar(30),
+	codeP 				numeric(5),
+	pays 				varchar(30) NOT NULL CONSTRAINT ck_pays,
 	tel 				numeric(10) UNIQUE NOT NULL,
-	email				varchar(45)
+	email				varchar(45) UNIQUE
+);
+
+CREATE TABLE Achete(
+	date_achat	date NOT NULL,
+	client		char(5),
+	magasin 	char(5),
+	produit 	char(5),
+	CONSTRAINT fk_client FOREIGN KEY (client) REFERENCES Client(idClient),
+	CONSTRAINT fk_magasin FOREIGN KEY (magasin) REFERENCES Magasin(idMagasin),
+	CONSTRAINT fk_prod_ach FOREIGN KEY (produit) REFERENCES Produit(idProduit)
 );
 
 CREATE TABLE Stock(
 	idStock 				char(5) PRIMARY KEY,
-	capacite	 			numeric(5) NOT NULL CONSTRAINT ck_capacite CHECK (capacite > 0),
-	pays 					varchar(30) NOT NULL CONSTRAINT ck_pays CHECK( NOT(pays = 'France' AND codeP IS NOT NULL)),
-	rue 					varchar(30) NOT NULL,
-	ville 					varchar(30) NOT NULL
-);
-
-CREATE TABLE Categorie(
-	idCategorie				char(5) PRIMARY KEY,
-	nom  					varchar(30) UNIQUE NOT NULL
-	CONSTRAINT ck_nom CHECK (nom IN ('Foot', 'Rugby', 'Natation', 'Boxe', 'Hokey', 'Tennis'))
-);
-
-CREATE TABLE Magasin(
-	idMagasin			char(5) PRIMARY KEY,
-	rue 				varchar(30) NOT NULL,
-	ville				varchar(40) NOT NULL,
-	pays 				varchar(42) NOT NULL CONSTRAINT ck_pays CHECK (NOT(pays = 'France' AND codeP IS NOT NULL)),
-	codeP				numeric(5) NOT NULL,
-	tel 				numeric(10) NOT NULL,
-	email 				varchar(45) NOT NULL
+	capacite	 			numeric(5) NOT NULL CONSTRAINT ck_cap CHECK (capacite >0),
+	rue 					varchar(30),
+	ville 					varchar(30),
+	codeP 					numeric(5),
+	pays 					varchar(30) NOT NULL CONSTRAINT ck_pays
 );
 
 CREATE TABLE Existe(
-	qte					numeric(5) NOT NULL,
-	idProduit			char(5),
-	idStock				char(5),
-	FOREIGN KEY (idProduit) REFERENCES Produit(idProduit),
-	FOREIGN KEY (idStock) REFERENCES Stock(idStock)
+	quantite			numeric(5) NOT NULL,
+	produit				char(5),
+	stock				char(5),
+	CONSTRAINT fk_prod_ex FOREIGN KEY (produit) REFERENCES Produit(idProduit),
+	CONSTRAINT fk_stock FOREIGN KEY (stock) REFERENCES Stock(idStock)
 );
 
-CREATE TABLE Approvisione(
-	qte					numeric(5) NOT NULL,
-	date_Ap				date NOT NULL,
-	idFournisseur		char(5),
-	idProduit			char(5),
-	FOREIGN KEY (idFournisseur) REFERENCES Fournisseur(idFournisseur),
-	FOREIGN KEY (idProduit) REFERENCES Produit(idProduit)
+CREATE TABLE Fournisseur(
+	idFournisseur 	char(5) PRIMARY KEY,
+	nom 			varchar(30) NOT NULL,
+	rue 			varchar(30),
+	ville 			varchar(40),
+	codeP			numeric(5),
+	pays 			varchar(42) NOT NULL CONSTRAINT ck_pays,
+	tel 			numeric(10) UNIQUE NOT NULL,
+	email			varchar(45) UNIQUE
 );
 
-CREATE TABLE Achete(
-	date_ach			date NOT NULL,
-	idClient			char(5),
-	idMagasin			char(5),
-	idProduit			char(5),
-	FOREIGN KEY (idClient) REFERENCES Client(idClient),
-	FOREIGN KEY (idMagasin) REFERENCES Magasin(idMagasin),
-	FOREIGN KEY (idProduit) REFERENCES Produit(idProduit)
+CREATE TABLE Approvisioner(
+	quantite			numeric(5) NOT NULL CONSTRAINT ck_qtte CHECK (quantite >0),
+	date_appro			date NOT NULL,
+	fournisseur			char(5),
+	produit				char(5),
+	CONSTRAINT fk_fourni FOREIGN KEY (fournisseur) REFERENCES Fournisseur(idFournisseur),
+	CONSTRAINT fk_prod_ap FOREIGN KEY (produit) REFERENCES Produit(idProduit)
 );

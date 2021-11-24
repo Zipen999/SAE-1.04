e7733574b096ac53c64ce4c50ebc315251825720
Axel wsh <92920034+NotVect0r@users.noreply.github.com>
2021-11-24 14:15:38 +0100

    Add files via upload

--- script.sql
+++ script.sql
@@ -1,25 +1,19 @@
 DROP TABLE Fournisseur;
-DROP TABLE Commande;
 DROP TABLE Produit;
 DROP TABLE Client;
 DROP TABLE Stock;
+DROP TABLE Categorie;
+DROP TABLE Magasin;
 
 CREATE TABLE Fournisseur(
 	idFournisseur 	char(5) PRIMARY KEY,
 	nom 			varchar(30) NOT NULL,
-	adresse 		varchar(30) NOT NULL,
+	rue 			varchar(30) NOT NULL,
 	ville 			varchar(40) NOT NULL,
 	pays 			char(42) NOT NULL,
 	codeP 			numeric(5) NOT NULL,
-	email			varchar(45) NOT NULL
-);
-
-CREATE TABLE Commande(
-	idCommande 			char(5) PRIMARY KEY,
-	idClient 			char(5) NOT NULL,
-	dateC 				date NOT NULL,
-	prix 				numeric(5,2) NOT NULL,
-	FOREIGN KEY (idClient) REFERENCES Client 
+	tel  			numeric(10) UNIQUE NOT NULL,
+	email			varchar(45)
 );
 
 CREATE TABLE Produit(
@@ -37,18 +31,37 @@ CREATE TABLE Client(
 	nom 				varchar(30) NOT NULL,
 	prenom 				varchar(30) NOT NULL,
 	date_naissance 		date NOT NULL,
-	adresse 			varchar(30) NOT NULL,
+	rue 				varchar(30) NOT NULL,
 	ville 				varchar(30) NOT NULL,
 	codeP 				numeric(5) NOT NULL,
 	pays 				varchar(30) NOT NULL,
-	tel 				numeric(10) NOT NULL,
+	tel 				numeric(10) UNIQUE NOT NULL,
 	email				varchar(45)
 );
 
 CREATE TABLE Stock(
 	idStock 				char(5) PRIMARY KEY,
-	espaceLibre 			numeric(5) NOT NULL,
+	capacite	 			numeric(5) NOT NULL,
 	pays 					varchar(30) NOT NULL,
-	adresse					varchar(30) NOT NULL,
+	rue 					varchar(30) NOT NULL,
 	ville 					varchar(30) NOT NULL
+);
+
+CREATE TABLE Categorie(
+	idCategorie				char(5) PRIMARY KEY,
+	nom  					varchar(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE Magasin(
+	idMagasin			char(5) PRIMARY KEY,
+	rue 				varchar(30) NOT NULL,
+	ville				varchar(40) NOT NULL,
+	pays 				varchar(42) NOT NULL,
+	codeP				numeric(5) NOT NULL,
+	tel 				numeric(10) NOT NULL,
+	email 				varchar(45) NOT NULL
+);
+
+CREATE TABLE Existe(
+
 );

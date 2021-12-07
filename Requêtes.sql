ae92b2e7ddc165a54f8e06c73aa8806ac675076d
Zipen999 <78689752+Zipen999@users.noreply.github.com>
2021-12-07 16:29:48 +0100

    Update Requêtes.sql

--- Requêtes.sql
+++ Requêtes.sql
@@ -1,16 +1,37 @@
 /* Question 1: */
-SELECT idProduit AS "Produits jamais commandés" FROM produit AS p LEFT JOIN Achete AS ac ON ac.produit = p.idProduit WHERE ac.produit  IS NULL;
+SELECT idProduit AS "Produits jamais commandés" 
+FROM produit AS p 
+LEFT JOIN Achete AS ac ON ac.produit = p.idProduit 
+WHERE ac.produit  IS NULL;
+
 /* Question 2: */
-SELECT 
+SELECT c.idClient AS "Client d'Auvergne", p.idProduit AS "Code produit" , round(p.prix*0.80,2) AS "Reduction de 20% sur les produit acheter en Auvergne"
+FROM Client AS c 
+INNER JOIN Achete AS a ON c.idClient = a.client
+INNER JOIN Produit AS p ON a.produit = p.idProduit
+WHERE c.departement='Auvergne' ;
+
 /* Question 3: */
-SELECT idClient AS "Client qui n'ont jamais commandés" FROM Client AS c LEFT JOIN Achete AS ac ON ac.client = c.idClient WHERE ac.client  IS NULL;
+SELECT idClient AS "Client qui n'ont jamais commandés" 
+FROM Client AS c 
+LEFT JOIN Achete AS ac ON ac.client = c.idClient 
+WHERE ac.client  IS NULL;
 
 /* Question 4: */
-SELECT Achete.quantite*Produit.prix AS "Fontcion à tester pour les client en UE" FROM Produit ,Achete ,Client
-WHERE ("Rajouter dans les tables un Truc UE que l'utilisateur entre tout seul")=='UE';
+SELECT SUM(p.prix*a.quantite) AS "Montant de vente totale en France"
+FROM Client AS c 
+INNER JOIN Achete AS a ON c.idClient = a.client
+INNER JOIN Produit AS p ON a.produit = p.idProduit
+WHERE c.region='FR';
 
-SELECT Achete.quantite*Produit.prix AS "Fontcion à tester pour les client hors UE" FROM Produit ,Achete ,Client
-WHERE ("Rajouter dans les tables un Truc UE que l'utilisateur entre tout seul")=='Hors UE';
+SELECT SUM(p.prix*a.quantite) AS "Montant de vente totale hors UE"
+FROM Client AS c 
+INNER JOIN Achete AS a ON c.idClient = a.client
+INNER JOIN Produit AS p ON a.produit = p.idProduit
+WHERE c.region='horsUE';
 
-SELECT Achete.quantite*Produit.prix AS "Fontcion à tester pour les client en UE" FROM Produit ,Achete ,Client
-WHERE Client.pays=='France' 
+SELECT SUM(p.prix*a.quantite) AS "Montant de vente totale UE"
+FROM Client AS c 
+INNER JOIN Achete AS a ON c.idClient = a.client
+INNER JOIN Produit AS p ON a.produit = p.idProduit
+WHERE c.region='UE';
